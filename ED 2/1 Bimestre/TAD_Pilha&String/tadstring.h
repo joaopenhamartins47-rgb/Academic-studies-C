42f48316cc461185ca82555ce006d2de57271ae3
João Vitor Penha <joao.penha.martins47@gmail.com>
2026-08-22 19:09:53 -0300

    remover com start e nro

--- ED 2/1 Bimestre/TAD_Pilha&String/tadstring.h
+++ ED 2/1 Bimestre/TAD_Pilha&String/tadstring.h
@@ -61,7 +61,12 @@ void insere(sd **inicio, char c)
 
 void copy(sd *inicio, sd **inicio2)
 {
-    sd *ultimo = NULL;
+    sd *ultimo = *inicio2;
+
+    if (ultimo)
+        while (ultimo->prox != NULL)
+            ultimo = ultimo->prox;
+    
 
     while(inicio)
     {
@@ -75,5 +80,49 @@ void copy(sd *inicio, sd **inicio2)
 
         ultimo = novo;
         inicio = inicio->prox;
+    }
+}
+
+
+void concatenar(sd *inicio, sd *inicio2, sd **inicio3)
+{
+    copy(inicio, inicio3);
+    copy(inicio2, inicio3);
+}
+
+//Remove da string str1, a quantidade de caracteres especificado por nro a partir da posição start;
+
+void remover(sd **inicio, int nro, int start)
+{
+    sd *ant, *aux;
+
+    if(start == 0)
+    {
+        while(*inicio != NULL && nro > 0)
+        {
+            aux = *inicio;
+            *inicio = (*inicio)->prox;
+            free(aux);
+            nro--;
+        }
+    }
+    else
+    {
+        ant = *inicio;
+        while(ant != NULL && start > 1) //Maior que 1 pq queremos parar uma casa antes dos elementos que serao deletados
+        {
+            ant = ant->prox;
+            start--;
+        }
+        if(ant)
+        {
+            while(ant->prox != NULL && nro > 0)
+            {
+                aux = ant->prox;
+                ant->prox = ant->prox->prox;
+                free(aux);
+                nro--;
+            }
+        }
     }
 }
